3672ce8328d2175692a34fff3af3efea8bd1c1c0
devjaeyoon <jaeyoon4819@gmail.com>
2023-04-28 09:51:48 +0900

    Chore: gatsby 설정 파일 추가
    gatsby가 제공하는 기능들과 플러그인을 사용하고 설정하기 위한 gatsby 설정 파일을 생성함

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: '이재윤 기술 블로그',
+    siteUrl: 'https://www.yourdomain.tld',
+  },
+  plugins: [],
+};
