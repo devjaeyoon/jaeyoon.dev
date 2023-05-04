3bda7ea81df4f65e84ade7bea6ee72f14d272e1f
devjaeyoon <jaeyoon4819@gmail.com>
2023-05-04 13:16:45 +0900

    Chore: 불필요한 props 코드 제거
    ThemeModeToggler에서 사용하는 props가 현재 없으므로 props 코드는 필요하지 않기 때문에 코드를 삭제함

--- src/components/ThemeModeToggler/ThemeModeToggler.jsx
+++ src/components/ThemeModeToggler/ThemeModeToggler.jsx
@@ -5,7 +5,7 @@ import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import MoonIcon from './MoonIcon';
 import SunIcon from './SunIcon';
 
-export default function ThemeModeToggler(props) {
+export default function ThemeModeToggler() {
   const { toggleColorMode } = useColorMode();
   const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
 
